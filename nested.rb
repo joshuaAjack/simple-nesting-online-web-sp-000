dafbcaba962e3a6a5a7b32dbdd09bfa878d60743
Joshua A Jack <mynameisjoshuajack@gmail.com>
2020-03-02 02:47:38 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -63,12 +63,7 @@ end
 
 def adding_matz
 
-programmer_hash[:yukihiro_matsumoto] = {
-  :known_for => "Ruby",
-    :languages => ["LISP", "C"]
-}
-
-	programmer_hash = 
+	programmer_hash =
  		{
         :grace_hopper => {
           :known_for => "COBOL",
@@ -83,7 +78,9 @@ programmer_hash[:yukihiro_matsumoto] = {
           :languages => ["C"]
         }
      }
-     programmer_hash[:yukihiro_matsumoto]
+
+programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
+programmer_hash
 end
 
 def changing_alan
